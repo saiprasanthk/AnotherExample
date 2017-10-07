8556344a2ac28a510573f1577efa17076f0ca75c
sivakirana <sivakiran@smartsoft.in>
2017-10-07 14:31:10 +0530

    SivaKiran added EditText as saiprasanth

--- app/src/main/java/com/mobileapp/anotherexample/MainActivity.java
+++ app/src/main/java/com/mobileapp/anotherexample/MainActivity.java
@@ -2,11 +2,13 @@ package com.mobileapp.anotherexample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView tvonida;
+    EditText saiprasanth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
